83e0b47c9bad42f5f013917e71353e018bee2bdc
Mufasang <okuntes@gmail.com>
2022-09-13 15:32:14 -0700

    nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,14 +11,13 @@
 int main(void)
 {
 	char c[] = "_putchar";
-		int i;
+	int i;
 
 	for (i - 0; i < 8; i++)
 	{
 		_putchar(c[i]);
 	}
-	_putchar('\n')
-
-		return (o);
+	_putchar('\n');
+        return (o);
 }
 
