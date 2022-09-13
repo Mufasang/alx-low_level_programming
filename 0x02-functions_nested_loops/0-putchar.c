bfbe2e41ec767181750a34f39f1125356f1d4cae
Mufasang <okuntes@gmail.com>
2022-09-13 15:14:55 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+ * main - check Description
+ *
+ * Descriptioon write a program that prints putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char nm = "Teslim"
+
+		while (*nm)
+		{
+			_putchar(*nm);
+			nm++'
+				_putchar('\n');
+
+			return (0);
+
