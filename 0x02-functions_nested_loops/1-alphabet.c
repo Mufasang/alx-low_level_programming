5b8add73753cce2e07c6f512aff6dbbdf41877d3
Mufasang <okuntes@gmail.com>
2022-09-14 00:14:11 -0700

    lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print lower case alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char a;
+	
+	for (a = 'a'; c <= 'z'; a++)
+		_putchar(a);
+	_putchar('\n');
+
+	return (0);
+}
