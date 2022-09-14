87c02773063909a7e6dd8c9796fefa537815c39c
Mufasang <okuntes@gmail.com>
2022-09-14 15:33:50 -0700

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,14 +11,14 @@ void jack_bauer(void)
 
 	for (H = 0; H < 24; H++)
 	{
-		for (M = O; M < 60; M++)
+		for (M = 0; M < 60; M++)
 		{
 			_putchar((H / 10) + '0');
 			_putchar((H % 10) + '0');
 			_putchar(':');
 			_putchar((M / 10) + '0');
-			_PUTCHAR((M % 10) + '0');
-			_PUTCHAR('\n');
+			_putchar((M % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
