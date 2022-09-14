cf01cf510288c350f0f6f3108b4ae762ecf301f7
Mufasang <okuntes@gmail.com>
2022-09-14 15:55:02 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -30,10 +30,10 @@ void times_table(void)
 			{
 				_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
-				if (j == 90)
+					if (j == 90)
 						continue;
 						_putchar(',');
-						_putchar(' ');
+					_putchar(' ');
 			}
 		}
 		_putchar('\n');
