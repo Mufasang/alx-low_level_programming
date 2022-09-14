162b2d8c300981d8211196f6081ff63172263366
Mufasang <okuntes@gmail.com>
2022-09-14 01:07:12 -0700

    lower case in 10s

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabetx10 - This code writes a function
+ * print_alphabet_x10 - This code writes a function
  * that prints 10 times the
  * alphabet in lowercase
  * Return: 0 on success
