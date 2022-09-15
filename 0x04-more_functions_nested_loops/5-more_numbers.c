713ec9cbf0d5181e0212b840130a003f772f1ded
Mufasang <okuntes@gmail.com>
2022-09-15 07:44:04 -0700

    more number

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - displays 0 to 14 x10
+ *
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (b = 0; b <= 9; b++)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			if (a > 9)
+			{
+				_putchar((digit / 10) + '0');
+			}
+			_putchar ((digit % 10)  + '0');
+		}
+		_putchar('\n');
+	}
+}
