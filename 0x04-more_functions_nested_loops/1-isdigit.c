4f6c20f071c60c28f7e43a10e9b0ce0064f7231c
Mufasang <okuntes@gmail.com>
2022-09-15 07:02:56 -0700

    digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -10,17 +10,10 @@
 
 int _isdigit(int c)
 {
-	char i '0';
-	int isdigit = 0;
-
-	for (; i <= '9'; i++)
+	if (c >= 48 && c <= 57)
 	{
-		if (i == c)
-		{
-			isdigit = 1;
-			break;
-		}
+		return (1);
 	}
-
-	return (isdigit);
+	else
+		return (0);
 }
