7a32bf5dad0e8e574be73da78548350dc86318a2
Mufasang <okuntes@gmail.com>
2022-09-15 07:24:23 -0700

    print number 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - a code that prints numbers 0 - 9
+ *
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 38;
+
+	while (i <= 47)
+	{
+		_putchar (i);
+		i++;
+	}
+	_putchar ('\n');
+}
