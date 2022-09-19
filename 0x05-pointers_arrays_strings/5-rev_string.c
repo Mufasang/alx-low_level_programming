4ce546b357cba597212b5dd03865404a2d79cb44
Mufasang <okuntes@gmail.com>
2022-09-19 12:25:43 -0700

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,13 +10,13 @@ void rev_string(char *a)
 	int len = 0, index = 0;
 	char tmp;
 
-	while (s[index++])
+	while (a[index++])
 		len++;
 
 	for (index = len - 1; index >= len / 2; index--)
 	{
-		tmp = s{index};
-		s[index] = s[len - index - 1];
-		s[len - index - 1] = tmp;
+		tmp = a[index];
+		a[index] = a[len - index - 1];
+		a[len - index - 1] = tmp;
 	}
 }
