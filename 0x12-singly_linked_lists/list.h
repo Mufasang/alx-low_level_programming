cc86a052f9ccea4ccfe3dc36f98846b33fe98766
Mufasang <okuntes@gmail.com>
2022-10-20 14:46:04 -0700

    task 0 done

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,7 +1,7 @@
-#ifndef _LISTS_
-#define _LISTS_
+#ifndef LINKED
+#define LINKED
 
-#include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -15,9 +15,9 @@
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
@@ -26,4 +26,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endif /*LINKED*/
